ca45b1159b0be442b04a369202009589900854e0
Nanoscience202 <vincentmingli@gmail.com>
2024-07-28 11:45:12 -0400

    feat: added tailwind main colors and font

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#EEF7FF",
+      black: "#15191C",
+      1: "#a2d2ff",
+      2: "#bde0fe",
+      3: "#ffafcc",
+      4: "#ffc8dd",
+      5: "#cdb4db",
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
