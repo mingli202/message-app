2eb506992fb9b4802ba766c5e9e47de48e3804fb
mingli202 <vincentmingli@gmail.com>
2024-10-07 21:38:37 -0400

    feat(frontend): basic signup/signin page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Login from "./Login";
+import Signup from "./Signup";
 
 export default function App() {
   const [state, setState] = useState<"login" | "signup" | "authenticated">(
     "login",
   );
+  const url = window.location.href.replace(/https?:\/\//, "");
+
+  useEffect(() => {
+    const path = url.split("/")[1];
+
+    if (path === "login" || path === "") {
+      setState("login");
+    } else if (path === "signup") {
+      setState("signup");
+    }
+  }, [url]);
 
   return (
     <>
       {state === "login" && <Login />}
-      {state === "signup" && <p>Signup</p>}
+      {state === "signup" && <Signup />}
       {state === "authenticated" && <p>Authenticated</p>}
     </>
   );
--- front-end/src/Signup.tsx
+++ front-end/src/Signup.tsx
@@ -1,26 +1,34 @@
 import { motion } from "framer-motion";
 
-export default function Login() {
+export default function Signup() {
   async function login(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    const res = await fetch("http://0.0.0.0:8000/auth/login", {
+    const password = e.currentTarget.password.value;
+    const confirmPassword = e.currentTarget.confirmPassword.value;
+
+    if (password !== confirmPassword) {
+      alert("Password does not match");
+      return;
+    }
+
+    const res = await fetch("http://0.0.0.0:8000/auth/signup", {
       method: "POST",
       body: new FormData(e.currentTarget),
     });
 
     if (!res.ok) {
+      console.log("erorr");
       return;
     }
 
-    const data = await res.json();
-    console.log(data);
+    console.log("success");
   }
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-white font-sans text-black">
       <div className="flex w-[30rem] flex-col justify-center gap-4 rounded-lg bg-[#FFF] p-4 shadow-[rgba(0,_0,_0,_0.16)_0px_10px_36px_0px,_rgba(0,_0,_0,_0.06)_0px_0px_0px_1px]">
-        <h1 className="p-1 text-4xl tracking-tight">Login</h1>
+        <h1 className="p-1 text-4xl tracking-tight">Sign Up</h1>
 
         <form className="flex flex-col gap-5" onSubmit={login}>
           <label htmlFor="username" className="flex flex-col">
@@ -43,24 +51,33 @@ export default function Login() {
               required
             />
           </label>
+          <label htmlFor="confirmPassword" className="flex flex-col">
+            <span className="p-2">Confirm Password</span>
+            <input
+              id="confirmPassword"
+              className="w-full rounded-full bg-1/20 p-2 outline-none transition focus:bg-1/30"
+              type="password"
+              required
+            />
+          </label>
 
           <p
             className="cursor-pointer hover:underline"
             onClick={() => {
-              window.location.assign("/signup");
+              window.location.assign("/login");
             }}
           >
-            Don't have an account? Sign Up!
+            Already have an account? Login!
           </p>
 
-          <LoginButton />
+          <SignupButton />
         </form>
       </div>
     </div>
   );
 }
 
-function LoginButton() {
+function SignupButton() {
   return (
     <motion.button
       type="submit"
@@ -77,7 +94,7 @@ function LoginButton() {
         shadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
       }}
     >
-      Login
+      Sign Up
     </motion.button>
   );
 }
